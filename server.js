c6530fd08398730e652d8263e9112eaa83fef364
nouserali <nouser.solanki@parasmesoft.com>
2022-11-20 10:39:15 +0530

    nouser ali

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import fs from "fs";
+import Router from "./Routers/Auth.route.js"
+import connectDB from "./config/db.js"
+import { config } from 'dotenv';
 
-import Router from "./Routers/test.route.js"
+config();
 const app = express()
 app.use(express.json())
-
+connectDB();
 app.use(Router);
 
-app.listen(3002,(request,response) =>{
+app.listen(process.env.PORT || 3002,(request,response) =>{
     console.log("Yes your server connected with PORT:3002");
 })
